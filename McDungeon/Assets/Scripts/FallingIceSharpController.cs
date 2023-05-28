28342c7aa14f0052ea61fd646ffdcbfb0aa884d2
honghui-li-8 <honghuili8@gmail.com>
2023-05-27 20:16:52 -0700

    IceSharpController

---
+++ McDungeon/Assets/Scripts/FallingIceSharpController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace McDungeon
+{
+    public class FallingIceSharpController : MonoBehaviour
+    {
+        [SerializeField] private Vector3 startPos;
+        [SerializeField] private Vector3 endPos;
+        [SerializeField] private float formTime;
+        [SerializeField] private float travelTime;
+        [SerializeField] private float lifeTime;
+        [SerializeField] private float targetScale;
+        private Vector3 direction;
+        private float speed;
+        private float timeSinceBorn;
+        private bool landed;
+        private ParticleSystemRenderer particelRenderer;
+        private CircleCollider2D collider2D;
+        private GameObject iceSharp;
+
+        const float startingScale = 0.02f;
+
+        void Start()
+        {
+            particelRenderer = this.gameObject.GetComponent<ParticleSystem>().GetComponent<ParticleSystemRenderer>();
+            collider2D = this.gameObject.GetComponent<CircleCollider2D>();
+            iceSharp = this.gameObject.transform.GetChild(0).gameObject;
+        }
+
+        public void Config(Vector3 startPos, Vector3 endPos, float formTime, float travelTime, float lifeTime, float targetScale)
+        {
+            this.startPos = startPos;
+            this.endPos = endPos;
+            this.formTime = formTime;
+            this.travelTime = formTime + travelTime;
+            this.lifeTime = formTime + travelTime + lifeTime;
+            this.targetScale = targetScale;
+            var distance = startPos - endPos;
+            distance.z = 0f;
+            direction = distance.normalized;
+            speed = distance.magnitude / travelTime;
+
+            // Initialzie Status.
+            timeSinceBorn = 0f;
+            landed = false;
+            this.transform.position = startPos;
+
+            Vector3 unitVec = new Vector3(1f, 1f, 1f);
+            this.transform.localScale = unitVec * startingScale;
+            particelRenderer.enabled = false;
+        }
+
+        void Update()
+        {
+            if (timeSinceBorn < formTime)
+            {
+                // Forming IceSharp.
+                Vector3 unitVec = new Vector3(1f, 1f, 1f);
+                var currentScale = Mathf.Lerp(startingScale, targetScale, timeSinceBorn / formTime);
+                this.transform.localScale = currentScale * unitVec;
+            }
+            else if (timeSinceBorn < travelTime)
+            {
+                // IceSharp Falling.
+                this.transform.position = this.transform.position + speed * direction * Time.deltaTime;
+            }
+            else if (landed == false)
+            {
+                // IceSharp Landed.
+                this.transform.position = endPos;
+                this.landed = true;
+
+                collider2D.enabled = true;
+                particelRenderer.enabled = true;
+                iceSharp.transform.localScale = new Vector3(0.05f, 0.2f, 0.2f);
+            }
+            else if (timeSinceBorn > lifeTime)
+            {
+                // IceSharp Expired.
+                Destroy(this.gameObject);
+            }
+
+
+            timeSinceBorn += Time.deltaTime;
+            // Debug.Log("timeSinceBorn" + timeSinceBorn + "    -    lifeTime" + lifeTime);
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+
+            if (other.gameObject.tag == "BouncyWall")
+            {
+            }
+        }
+    }
+}
