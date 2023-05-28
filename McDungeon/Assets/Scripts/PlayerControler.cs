1bd4acd4a4ec9758728e91664f43574bc1d0ac31
honghui-li-8 <honghuili8@gmail.com>
2023-05-27 19:23:20 -0700

    change to McDungeon namespace

--- McDungeon/Assets/Scripts/PlayerControler.cs
+++ McDungeon/Assets/Scripts/PlayerControler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Obscura
+namespace McDungeon
 {
     public class PlayerController : MonoBehaviour
     {
