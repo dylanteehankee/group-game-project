e8b48c2fdb0896c9194e2433fc5e55075aaea0dd
honghui-li-8 <honghuili8@gmail.com>
2023-06-01 23:11:57 -0700

    water surge maker

---
+++ McDungeon/Assets/Scripts/SpellScripts/WaterSurgeMaker.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace McDungeon
+{
+    public class WaterSurgeMaker : MonoBehaviour, ISpellMaker
+    {
+        [SerializeField] private GameObject prefab_waterSurge;
+        private float range;
+
+
+        public void Activate()
+        {
+
+        }
+
+        public void ChangeRange(float radius)
+        {
+            this.range = radius;
+        }
+
+        public void ShowRange(Vector3 posistion, Vector3 mousePos)
+        {
+            Debug.Log("Spell Aiming");
+        }
+
+
+        public GameObject Execute(Vector3 posistion, Vector3 mousePos)
+        {
+            // Instantiate spell
+            Vector3 spellDir = (mousePos - posistion).normalized;
+            GameObject waterSurge = Instantiate(prefab_waterSurge, posistion + spellDir, Quaternion.identity);
+            waterSurge.GetComponent<FireBallController>().Config(1f, 10f, 2f, spellDir);
+
+            // Debug.Log("spellDir: " + spellDir);
+
+            return waterSurge;
+        }
+    }
+}
