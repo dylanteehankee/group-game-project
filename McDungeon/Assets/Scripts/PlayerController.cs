bc6f5fc4acff19a8b6bfeecea192b2496cd58fd8
oycheng <123531322+oycheng@users.noreply.github.com>
2023-06-07 00:34:18 -0700

    Scene and Player change to test Statuses

--- McDungeon/Assets/Scripts/PlayerController.cs
+++ McDungeon/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ namespace Mobs
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private float speed = 10.0f;
+        [SerializeField] private GameObject target;
         private Vector3 movementDirection;
         // Start is called before the first frame update
 
@@ -15,11 +16,17 @@ namespace Mobs
         {
             if (Input.GetButtonDown("Jump"))
             {
-                GameObject[] spawner = GameObject.FindGameObjectsWithTag("MobSpawner");
-                if (spawner.Length > 0)
+                GameObject[] targets = GameObject.FindGameObjectsWithTag("MobHitbox");
+                if (targets.Length > 0)
                 {
-                    spawner[0].GetComponent<MobManager>().SpawnMobs((MobTypes)Random.Range(0, 5));
+                    target = targets[0];
+                    target.GetComponent<IMobController>().TakeDamage(1, EffectTypes.Freeze);
                 }
+                // GameObject[] spawner = GameObject.FindGameObjectsWithTag("MobSpawner");
+                // if (spawner.Length > 0)
+                // {
+                //     spawner[0].GetComponent<MobManager>().SpawnMobs((MobTypes)Random.Range(0, 5));
+                // }
             }
 
             movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
