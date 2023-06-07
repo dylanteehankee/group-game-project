918f48e7f9004435ea5bd4346701d220003deb4b
oycheng <123531322+oycheng@users.noreply.github.com>
2023-06-07 00:31:40 -0700

    Statuses Implementation

---
+++ McDungeon/Assets/Scripts/EffectTypes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobs
+{
+    public enum EffectTypes
+    {
+        None,
+        Slow,
+        Freeze,
+        Ablaze
+    }
+}
