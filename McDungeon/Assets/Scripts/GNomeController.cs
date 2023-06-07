22570ff3e6d8bd6c90e63a10f4e195a9173ced98
oycheng <123531322+oycheng@users.noreply.github.com>
2023-06-07 00:32:28 -0700

    Most GNome Implementation

--- McDungeon/Assets/Scripts/GNomeController.cs
+++ McDungeon/Assets/Scripts/GNomeController.cs
@@ -7,66 +7,70 @@ namespace Mobs
     public class GNomeController : MonoBehaviour, IMobController
     {
         [SerializeField]
-        private float mobHealth = 20;
+        private GameObject playerObject;
         [SerializeField]
-        private int attackRange = 5;
+        private StatusEffects statusEffects;
         [SerializeField]
-        private float attackSpeed = 2f;
+        private GameObject knifePrefab;
         [SerializeField]
-        private float castSpeed = 10f;
+        private GameObject gNelfPrefab;
         [SerializeField]
-        private float castTime = 4f;
+        private float mobHealth = 20.0f;
         [SerializeField]
-        private float moveSpeed = 1f;
+        private float attackRange = 5.0f;
         [SerializeField]
-        private float hitStun = 0.25f;
-        private float elapsedStun = 0f;
-        private float attackCooldown = 0f;
-        private float castCooldown = 0f;
-        private float castDelayTime = 0f;
-        private float throwTime = 0.8f;
-        private float elapsedThrowTime = 0f;
+        private float attackSpeed = 2.0f;
+        private float attackCD = 0.0f;
+        private const float THROWDURATION = 0.8f;
+        private float elapsedThrowTime = 0.0f;
         private bool isThrowing = false;
         private bool hasKnife = false;
         private bool isCasting = false;
         [SerializeField]
-        private GameObject playerObject;
+        private float castSpeed = 10f;
+        private float castCD = 0.0f;
         [SerializeField]
-        private GameObject gNelfPrefab;
+        private float castTime = 3.6f;
+        private float elapsedCastTime = 0.0f;
         [SerializeField]
-        private GameObject potionDropPrefab;
+        private float moveSpeed = 1.0f;
+        private float speedModifier = 1.0f;
         [SerializeField]
-        private GameObject knifePrefab;
+        private float stunDuration = 1.0f;
+        private bool stunned = false;
+        private bool isAblaze = false;
+        private bool isFreeze = false;
+        private GameObject stunObject;
+        private GameObject ablazeObject;
+        private GameObject freezeObject;
         private SpriteRenderer spriteRenderer;
         private Animator animator;
-
         void Start()
         {
             this.spriteRenderer = this.GetComponent<SpriteRenderer>();
             this.animator = this.GetComponent<Animator>();
         }
+
         void Update()
         {
-            Vector2 location = this.transform.position;
-            Vector2 playerLocation = this.playerObject.transform.position;
-
-            this.attackCooldown += Time.deltaTime;
-            this.castCooldown += Time.deltaTime;
-            if (this.elapsedStun < hitStun)
+            if (!this.stunned && !this.isFreeze)
             {
-                this.elapsedStun += Time.deltaTime;
-            }
-            else if (!isThrowing && (this.castCooldown > this.castSpeed || isCasting))
-            {
-                this.casting();
-            }
-            else if((Vector2.Distance(location, playerLocation) < this.attackRange && this.attackCooldown > this.attackSpeed) || isThrowing)
-            {
-                this.throwKnife(playerLocation - location);
-            }
-            else
-            {
-                moveTowardPlayer(playerLocation - location);
+                Vector2 location = this.transform.position;
+                Vector2 playerLocation = this.playerObject.transform.position;
+                this.attackCD += Time.deltaTime;
+                this.castCD += Time.deltaTime;
+                if (!isThrowing && (this.castCD > castSpeed || isCasting))
+                {
+                    this.casting();
+                }
+                else if((Vector2.Distance(location, playerLocation) < this.attackRange && this.attackCD > this.attackSpeed) || isThrowing)
+                {
+                    this.throwKnife(playerLocation - location);
+                }
+                else
+                {
+                    moveTowardPlayer(playerLocation - location);
+                }
             }
         }
 
@@ -83,7 +87,7 @@ namespace Mobs
         private void moveTowardPlayer(Vector2 deltaLocation)
         {
             deltaLocation.Normalize();
-            this.transform.Translate(deltaLocation * Time.deltaTime * moveSpeed);
+            this.transform.Translate(deltaLocation * Time.deltaTime * moveSpeed * speedModifier);
             this.spriteDirection(deltaLocation);
         }
 
@@ -91,16 +95,17 @@ namespace Mobs
         {
             if (this.elapsedThrowTime == 0)
             {
+                this.spriteDirection(deltaLocation);
                 this.transform.Translate(Vector2.zero);
                 this.animator.SetTrigger("Attack");
                 isThrowing = true;
                 hasKnife = true;
             }
-            else if (this.elapsedThrowTime > this.throwTime)
+            else if (this.elapsedThrowTime > THROWDURATION)
             {
                 this.isThrowing = false;
                 this.elapsedThrowTime = 0;
-                this.attackCooldown = 0;
+                this.attackCD = 0;
                 return;
             }
             else if (this.elapsedThrowTime > 0.6 && hasKnife)
@@ -121,21 +126,21 @@ namespace Mobs
             if (!isCasting)
             {
                 Debug.Log("CASTING");
-                this.animator.SetTrigger("Cast");
+                this.animator.SetBool("Cast", true);
                 isCasting = true;
             }
-            else if (this.castDelayTime > this.castTime)
+            else if (this.elapsedCastTime > castTime)
             {
                 var spawner = this.transform.parent.gameObject.GetComponent<MobManager>();
                 spawner.SpawnGNelfs(this.gNelfPrefab, this.transform.position);
-                this.castDelayTime = 0;
-                this.castCooldown = 0;
+                this.elapsedCastTime = 0;
+                this.castCD = 0;
                 this.isCasting = false;
-                this.animator.SetTrigger("Cast");
+                this.animator.SetBool("Cast", false);
                 return;
             }
 
-            this.castDelayTime += Time.deltaTime;
+            this.elapsedCastTime += Time.deltaTime;
         }
 
         private void spriteDirection(Vector2 deltaLocation)
@@ -162,21 +167,102 @@ namespace Mobs
             }
         }
 
-        public void TakeDamage(float damage)
+        public void TakeDamage(float damage, EffectTypes type)
         {
             this.mobHealth -= damage;
-            if (this.mobHealth < 0)
+            this.death();
+            this.stunned = true;
+            this.status(type);
+            StopCoroutine("stunStatus");
+            StartCoroutine("stunStatus");         
+        }
+
+        private void death()
+        {
+            if (this.mobHealth <= 0)
             {
+                statusEffects.Death(this.gameObject.transform.position, Vector2.one);
                 Destroy(this.gameObject);
                 return;
             }
-            this.elapsedStun = 0;
+        }
+
+        private IEnumerator stunStatus()
+        {
+            if (!this.stunObject)
+            {
+                this.stunObject = statusEffects.Stun(this.transform, Vector2.one, new Vector2 (0, 0.5f));
+            }
             this.isThrowing = false;
-            this.elapsedThrowTime = 0;
-            this.attackCooldown = 0;
-            this.castDelayTime = 0;
-            this.castCooldown = 0;
             this.isCasting = false;
+            this.animator.SetBool("Cast", false);
+            this.animator.SetBool("Stun", true);
+            yield return new WaitForSeconds(stunDuration);
+            this.animator.SetBool("Stun", false);
+            this.elapsedThrowTime = 0;
+            this.attackCD = Mathf.Min(this.attackCD, attackSpeed * 0.8f);
+            this.elapsedCastTime = 0;
+            this.castCD = 0;
+            this.stunned = false;
+            Destroy(this.stunObject);
+        }
+
+        private void status(EffectTypes type)
+        {
+            switch (type)
+            {
+                case EffectTypes.None:
+                    break;
+                case EffectTypes.Ablaze:
+                    if (!this.ablazeObject)
+                    {
+                        this.ablazeObject = this.statusEffects.Ablaze(this.transform, Vector2.one, Vector2.zero);
+                        this.isAblaze = true;
+                    }
+                    StopCoroutine("ablazeStatus");
+                    StartCoroutine("ablazeStatus");
+                    break;
+                case EffectTypes.Freeze:
+                    if (!this.freezeObject)
+                    {
+                        this.freezeObject = this.statusEffects.Freeze(this.transform, new Vector2(2, 1.5f), new Vector2(0, -0.1f));
+                        this.animator.SetBool("Freeze", true);
+                        this.isFreeze = true;
+                    }
+                    StopCoroutine("freezeStatus");
+                    StartCoroutine("freezeStatus");
+                    break;
+                case EffectTypes.Slow:
+                    StopCoroutine("slowStatus");
+                    StartCoroutine("slowStatus");
+                    break;
+            }
+        }
+        private IEnumerator ablazeStatus()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                yield return new WaitForSeconds(this.statusEffects.GetAblazeDuration() / 4);
+                this.mobHealth -= this.statusEffects.GetAblazeDamage();
+                this.death();
+            }
+            this.isAblaze = false;
+            Destroy(this.ablazeObject);
+        }
+
+        private IEnumerator freezeStatus()
+        {
+            yield return new WaitForSeconds(this.statusEffects.GetFreezeDuration());
+            this.isFreeze = false;
+            this.animator.SetBool("Freeze", false);
+            Destroy(this.freezeObject);
+        }
+
+        private IEnumerator slowStatus()
+        {
+            this.speedModifier = this.statusEffects.GetSlowModifier();
+            yield return new WaitForSeconds(this.statusEffects.GetSlowDuration());
+            this.speedModifier = 1.0f;
         }
     }
 }
