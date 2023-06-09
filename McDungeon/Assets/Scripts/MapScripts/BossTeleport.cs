e288b5db7cde232a37077ed63f9980f3dd75dc65
paoloyap22 <123781349+paoloyap22@users.noreply.github.com>
2023-06-09 03:53:01 -0700

    Updated Boss Room

---
+++ McDungeon/Assets/Scripts/MapScripts/BossTeleport.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTeleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject ventExit;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.CompareTag("Player")){
+            other.transform.position = ventExit.transform.position;
+        }
+    }
+}
