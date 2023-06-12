8a8a2c480873c8ce4de23acc264233867fde6164
Honghui-Li-8 <honghuili8@gmail.com>
2023-06-11 23:32:45 -0700

    camera mode

--- McDungeon/Assets/Scripts/CameraScripts/CameraControl.cs
+++ McDungeon/Assets/Scripts/CameraScripts/CameraControl.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 namespace McDungeon
 {
+    enum CameraMode
+    {
+        LockOnPlayer,
+        LockOnRoom
+    }
+
     public class PositionLockCamera : MonoBehaviour
     {
         private Camera managedCamera;
-        [SerializeField]
-        protected GameObject Target;
-        
+        [SerializeField] protected GameObject Target;
+        [SerializeField] protected float returnSpeed;
+        [SerializeField] protected CameraMode cameraMode;
+        [SerializeField] protected Vector3 roomCenter;
+
 
         private void Awake()
         {
             managedCamera = gameObject.GetComponent<Camera>();
+            returnSpeed = 6f;
         }
 
         //Use the LateUpdate message to avoid setting the camera's position before
         //GameObject locations are finalized.
-        void LateUpdate()
+        void FixedUpdate()
         {
-            var targetPosition = this.Target.transform.position;
-            var cameraPosition = managedCamera.transform.position;
-
-            if (targetPosition.y != cameraPosition.y || targetPosition.x != cameraPosition.x)
+            if (cameraMode == CameraMode.LockOnPlayer)
             {
-                cameraPosition = new Vector3(targetPosition.x, targetPosition.y, cameraPosition.z);
-            }
+                var targetPosition = this.Target.transform.position;
+                var cameraPosition = managedCamera.transform.position;
 
-            managedCamera.transform.position = cameraPosition;
+                Vector3 distance = targetPosition - cameraPosition;
+                distance.z = 0f;
+                if ((distance).magnitude > 0.5f)
+                {
+                    // Just switch back to position lock Cam, move camera twords player.
+                    Vector3 direction = distance.normalized;
+                    managedCamera.transform.Translate(direction * returnSpeed * Time.fixedDeltaTime);
+                }
+                else if (targetPosition.y != cameraPosition.y || targetPosition.x != cameraPosition.x)
+                {
+                    cameraPosition = new Vector3(targetPosition.x, targetPosition.y, cameraPosition.z);
+                    managedCamera.transform.position = cameraPosition;
+                }
+            }
+            else
+            {
+                managedCamera.transform.position = roomCenter;
+            }
+        }
+
+        void changeCameraMode(CameraMode mode, Vector2 center)
+        {
+            cameraMode = mode;
+            roomCenter = new Vector3(center.x, center.y, managedCamera.transform.position.z);
         }
     }
 }
