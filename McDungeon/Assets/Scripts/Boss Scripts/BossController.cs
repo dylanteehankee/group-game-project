aa0fd210263bd3622a6443f569b677165cbed49f
oycheng <oriencheng@gmail.com>
2023-06-13 06:07:34 -0700

    Boss Implementation

--- McDungeon/Assets/Scripts/Boss Scripts/BossController.cs
+++ McDungeon/Assets/Scripts/Boss Scripts/BossController.cs
@@ -13,11 +13,19 @@ public class BossController : MonoBehaviour
     [SerializeField]
     private GameObject handRight;
     [SerializeField]
+    private GameObject buttonObject;
+    [SerializeField]
+    private GameObject laserObject;
+    [SerializeField]
     private float bossHealth = 100.0f;
+    private float moveSpeed = 0.5f;
     private float laserDamage = 2.0f;
     private float attackSpeed = 5.0f;
     private float attackCD = 0.0f;
     private int attackCycle = 0;
+    private bool isAttack = false;
+    private float handAttackTime = 3.0f;
+    private float laserAttackTime = 3.5f;
     private bool isAblaze = false;
     private GameObject ablazeObject;
     private float deathAnimationTime = 5.0f;
@@ -32,43 +40,108 @@ public class BossController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (this.attackCD < this.attackSpeed)
+        if (this.attackCD < this.attackSpeed && !isAttack)
         {
             this.attackCD += Time.fixedDeltaTime;
+            this.moveTowardPlayer();
         }
-        else
+        else if (!isAttack)
         {
             this.attackPlayer();
         }
     }
 
-    private void attackPlayer()
+    private void moveTowardPlayer()
     {
-        if (this.attackCD >= this.attackSpeed)
+        Vector2 location = this.transform.position;
+        Vector2 playerLocation = this.playerObject.transform.position;
+        Vector2 deltaLocation = playerLocation - location;
+        if (deltaLocation.x > 0)
         {
-            if (attackCycle > 2)
+            this.transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+            if (deltaLocation.x < 0.05)
             {
-                attackCycle += 1;
-                int hand = Random.Range(0, 2);
-                if (hand == 0)
-                {
-                    this.handLeft.GetComponent<BossHandController>().Attack(playerObject);
-                }
-                else
-                {
-                    this.handRight.GetComponent<BossHandController>().Attack(playerObject);
-                }
+                this.animator.SetBool("Moving", false);
             }
             else
             {
-                this.attackLaser();
+                this.animator.SetBool("Moving", true);
+                this.spriteRenderer.flipX = true;
+            }
+        }
+        else if (deltaLocation.x < 0)
+        {
+            this.transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+            if (deltaLocation.x > -0.05)
+            {
+                this.animator.SetBool("Moving", false);
+            }
+            else
+            {
+                this.animator.SetBool("Moving", true);
+                this.spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            this.animator.SetBool("Moving", false);
+        }
+    }
+
+    private void attackPlayer()
+    {
+        if (!isAttack)
+        {
+            isAttack = true;
+            this.animator.SetBool("Attack", true);
+            if (attackCycle < 2)
+            {
+                attackCycle += 1;
+                Vector2 location = this.transform.position;
+                Vector2 playerLocation = this.playerObject.transform.position;
+                Vector2 deltaLocation = playerLocation - location;
+                this.buttonObject.GetComponent<ButtonController>().Execute(this.playerObject);
+                BossHandController handControl;
+                if (deltaLocation.x >= 0)
+                {
+                    handControl = this.handRight.GetComponent<BossHandController>();
+                    Debug.Log("ATTACKRIGHT");
+                }
+                else
+                {
+                    handControl = this.handLeft.GetComponent<BossHandController>();
+                    Debug.Log("ATTACKLEFT");
+                }
+                StartCoroutine(attackHand(handControl));
+            }
+            else
+            {
+                attackCycle = 0;
+                StartCoroutine("attackLaser");
             }
         }
     }
 
-    private void attackLaser()
+    private IEnumerator attackLaser()
     {
-        //LASER
+        this.animator.SetBool("Laser", true);
+        yield return new WaitForSeconds(0.25f);
+        this.laserObject.GetComponent<LaserController>().Execute();
+        yield return new WaitForSeconds(this.laserAttackTime);
+        this.animator.SetBool("Laser", false);
+        this.animator.SetBool("Attack", false);
+        this.isAttack = false;
+        this.attackCD = 0;
+    }
+
+    private IEnumerator attackHand(BossHandController handControl)
+    {
+        yield return new WaitForSeconds(2.5f);
+        handControl.Execute(this.playerObject);
+        yield return new WaitForSeconds(1f);
+        this.isAttack = false;
+        this.animator.SetBool("Attack", false);
+        this.attackCD = 0;
     }
     public virtual void TakeDamage(float damage, EffectTypes type)
     {
