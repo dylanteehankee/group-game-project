e30049d0dd69e5464f9ae13a25f1809c39e574b2
oycheng <oriencheng@gmail.com>
2023-06-13 06:03:25 -0700

    Boss Assets

---
+++ McDungeon/Assets/Scripts/Boss Scripts/LaserController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using McDungeon;
+public class LaserController : MonoBehaviour
+{
+    private GameObject playerObject;
+    private float attackTime = 4.0f;
+    private int damage = 1;
+    // Start is called before the first frame update
+    public void Execute()
+    {
+        this.gameObject.SetActive(true);
+        this.GetComponent<Animator>().SetTrigger("Start");
+        StartCoroutine("laserCD");
+    }
+    
+    private IEnumerator laserCD()
+    {
+        yield return new WaitForSeconds(3.0f);
+        this.GetComponent<Animator>().SetTrigger("Dissipate");
+        yield return new WaitForSeconds(0.5f);
+        this.gameObject.SetActive(false);
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "PlayerHitbox")
+        {
+            Debug.Log("HIT");
+            collider.gameObject.GetComponent<PlayerController>().TakeDamage(damage, EffectTypes.None);
+        }
+    }
+}
